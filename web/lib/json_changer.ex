4b0a0d0509b6250beebcb1d833c69d3c5f3be8ed
Stefan Mijucic <fanna993@gmail.com>
2016-03-13 18:03:19 +0100

    Add new random functions

--- web/lib/json_changer.ex
+++ web/lib/json_changer.ex
@@ -1,10 +1,25 @@
 defmodule ServerReactBack.JsonChanger do
   def change(data) do
-    data = %{"body" => random()}
+    data = %{"status" => random_status(), "rack" => random_rack(), "server" => random_server(), "cpu" => random_cpu()}
   end
 
-  defp random() do
+  defp random_status() do
     list = [true, false]
+    Enum.random(list)
+  end
+
+  defp random_rack() do
+    list = [0, 1, 2, 3, 4, 5]
+    Enum.random(list)
+  end
+
+  defp random_server() do
+    list = [0, 1, 2, 3, 4, 5]
+    Enum.random(list)
+  end
+
+  defp random_cpu() do
+    list = [0, 1, 2, 3]
     Enum.random(list)
   end
 end
